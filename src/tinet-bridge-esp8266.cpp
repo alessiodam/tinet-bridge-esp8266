34299fd933f9830279d456db56c5a89b0a9a4089
TKB Studios <69647028+tkbstudios@users.noreply.github.com>
2024-01-07 17:32:10 +0100

    formatted code

--- src/tinet-bridge-esp8266.cpp
+++ src/tinet-bridge-esp8266.cpp
@@ -20,10 +20,10 @@
 
 #define NO_OTA_NETWORK
 
-const uint8_t YELLOW_LED = 5;   // D1 on NodeMCU
-const uint8_t BLUE_LED = 4;     // D2 on NodeMCU
-const uint8_t GREEN_LED = 14;   // D5 on NodeMCU
-const uint8_t RED_LED = 12;     // D6 on NodeMCU
+const uint8_t YELLOW_LED = 5; // D1 on NodeMCU
+const uint8_t BLUE_LED = 4;   // D2 on NodeMCU
+const uint8_t GREEN_LED = 14; // D5 on NodeMCU
+const uint8_t RED_LED = 12;   // D6 on NodeMCU
 
 WiFiClient wifi_client;
 int wifi_status = WL_IDLE_STATUS;
@@ -44,7 +44,8 @@ DNSServer dnsServer;
 IPAddress apIP(192, 168, 1, 1);
 IPAddress netMsk(255, 255, 255, 0);
 
-struct Settings {
+struct Settings
+{
   char wifi_ssid[32];
   char wifi_pass[64];
   char password[64];
@@ -53,7 +54,6 @@ struct Settings {
 };
 
 Settings settings;
-
 
 void handleSetupRoot();
 void handleSetupSaveConfig();
@@ -70,8 +70,8 @@ void saveSettings();
 void resetToFactorySettings();
 void flashLED(uint8_t led_to_flash, int delay_to_flash);
 
-
-void setup() {
+void setup()
+{
   Serial.begin(SERIAL_BAUDRATE);
   Serial.setTimeout(1000);
   EEPROM.begin(sizeof(Settings));
@@ -92,13 +92,14 @@ void setup() {
   digitalWrite(GREEN_LED, LOW);
   digitalWrite(YELLOW_LED, LOW);
   digitalWrite(RED_LED, LOW);
-  
+
   loadSettings();
 
   float eepromdataaddresszero;
   EEPROM.get(0, eepromdataaddresszero);
 
-  if (strlen(settings.wifi_ssid) == 0 || strlen(settings.wifi_pass) == 0 || isnan(eepromdataaddresszero) || settings.boot_setup_mode == true) {
+  if (strlen(settings.wifi_ssid) == 0 || strlen(settings.wifi_pass) == 0 || isnan(eepromdataaddresszero) || settings.boot_setup_mode == true)
+  {
     digitalWrite(YELLOW_LED, HIGH);
     digitalWrite(GREEN_LED, HIGH);
     Serial.println("BRIDGE_SET_UP_WIFI");
@@ -111,23 +112,27 @@ void setup() {
     /* Setup the DNS server redirecting all the domains to the apIP */
     dnsServer.setErrorReplyCode(DNSReplyCode::NoError);
     dnsServer.start(DNS_PORT, "*", apIP);
-    
+
     server.on("/", HTTP_GET, handleSetupRoot);
     server.on("/saveconfig", HTTP_POST, handleSetupSaveConfig);
     server.on("/reset", HTTP_POST, handleReset);
-    server.on("/generate_204", handleSetupRoot);  // Android captive portal. Maybe not needed. Might be handled by notFound handler.
-    server.on("/fwlink", handleSetupRoot);        // Microsoft captive portal. Maybe not needed. Might be handled by notFound handler.
-    server.onNotFound(handleSetupRoot);           // idk testing things out :p
+    server.on("/generate_204", handleSetupRoot); // Android captive portal. Maybe not needed. Might be handled by notFound handler.
+    server.on("/fwlink", handleSetupRoot);       // Microsoft captive portal. Maybe not needed. Might be handled by notFound handler.
+    server.onNotFound(handleSetupRoot);          // idk testing things out :p
 
-    if (!MDNS.begin("tinetbridge")) {
+    if (!MDNS.begin("tinetbridge"))
+    {
       flashLED(RED_LED, 500);
-    } else {
+    }
+    else
+    {
       MDNS.addService("http", "tcp", 80);
     }
-    
+
     server.begin();
-    
-    while (strlen(settings.wifi_ssid) == 0 || strlen(settings.wifi_pass) == 0 || isnan(eepromdataaddresszero) || settings.boot_setup_mode == true) {
+
+    while (strlen(settings.wifi_ssid) == 0 || strlen(settings.wifi_pass) == 0 || isnan(eepromdataaddresszero) || settings.boot_setup_mode == true)
+    {
       dnsServer.processNextRequest();
       server.handleClient();
     }
@@ -138,8 +143,9 @@ void setup() {
   unsigned long wifi_connect_current_time = millis();
   unsigned long wifi_connect_elapsed_time = 0;
   wifi_status = WiFi.begin(settings.wifi_ssid, settings.wifi_pass);
-  
-  while (wifi_status != WL_CONNECTED) {
+
+  while (wifi_status != WL_CONNECTED)
+  {
     wifi_status = WiFi.status();
     digitalWrite(GREEN_LED, HIGH);
     delay(100);
@@ -147,18 +153,19 @@ void setup() {
     delay(100);
     wifi_connect_current_time = millis();
     wifi_connect_elapsed_time = wifi_connect_current_time - wifi_connect_start_time;
-    if (wifi_connect_elapsed_time >= 10000) {
+    if (wifi_connect_elapsed_time >= 10000)
+    {
       digitalWrite(GREEN_LED, LOW);
       digitalWrite(RED_LED, HIGH);
       Serial.println("BRIDGE_REBOOT_TO_SETUP");
       settings.boot_setup_mode = true;
       saveSettings();
-      
+
       delay(2000);
       ESP.restart();
     }
   }
-  
+
   Serial.println("WIFI_CONNECTED");
   digitalWrite(GREEN_LED, LOW);
   flashLED(GREEN_LED, 200);
@@ -167,7 +174,7 @@ void setup() {
 
   timeClient.begin();
   timeClient.update();
-  
+
   server.on("/", HTTP_GET, handleRoot);
   server.on("/setpassword", HTTP_GET, handleSetPasswordPage);
   server.on("/savepassword", HTTP_POST, handleSavePassword);
@@ -178,43 +185,51 @@ void setup() {
   connectToTCPServer();
 }
 
-void loop() {
+void loop()
+{
   server.handleClient();
-  if (tcp_client.connected()) {
+  if (tcp_client.connected())
+  {
     handleSerialToTCP();
     handleTCPToSerial();
-  } else if (!tcp_client.connected()) {
+  }
+  else if (!tcp_client.connected())
+  {
     handleSerialToTCP();
   }
 }
 
-void handleSetupRoot() {
+void handleSetupRoot()
+{
   flashLED(GREEN_LED, 10);
-  
+
   server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
   server.sendHeader("Pragma", "no-cache");
   server.sendHeader("Expires", "-1");
-  
+
   server.send(200, "text/html", SETUP_ROOT_PAGE_HTML);
 }
 
-void handleSetupSaveConfig() {
+void handleSetupSaveConfig()
+{
   flashLED(GREEN_LED, 10);
-  
+
   String newSSID = server.arg("ssid").c_str();
-  String newPassword = server.arg("password").c_str();;
+  String newPassword = server.arg("password").c_str();
+  ;
   newSSID.toCharArray(settings.wifi_ssid, sizeof(settings.wifi_ssid));
   newPassword.toCharArray(settings.wifi_pass, sizeof(settings.wifi_pass));
   settings.boot_setup_mode = false;
-  
-  saveSettings();  
+
+  saveSettings();
 
   server.send(200, "text/html", SETUP_SAVE_CONFIG_HTML);
   delay(200);
   ESP.restart();
 }
 
-void handleReset() {
+void handleReset()
+{
   flashLED(GREEN_LED, 10);
   digitalWrite(RED_LED, HIGH);
   resetToFactorySettings();
@@ -225,21 +240,27 @@ void handleReset() {
   ESP.restart();
 }
 
-void handleRoot() {
+void handleRoot()
+{
   flashLED(GREEN_LED, 10);
-  if (strlen(settings.password) == 0) {
+  if (strlen(settings.password) == 0)
+  {
     server.send(200, "text/html", ROOT_NO_PASSWORD_HTML);
-  } else {
+  }
+  else
+  {
     server.send(200, "text/html", ROOT_HTML);
   }
 }
 
-void handleSetPasswordPage() {
+void handleSetPasswordPage()
+{
   flashLED(GREEN_LED, 10);
   server.send(200, "text/html", SET_PASSWORD_HTML);
 }
 
-void handleSavePassword() {
+void handleSavePassword()
+{
   flashLED(GREEN_LED, 10);
   String newPassword = server.arg("password").c_str();
   newPassword.toCharArray(settings.password, sizeof(settings.password));
@@ -247,10 +268,11 @@ void handleSavePassword() {
   server.send(200, "text/html", SET_PASSWORD_SUCCESS_HTML);
 }
 
-void handleUpdate() {
+void handleUpdate()
+{
   // IN DEVELOPMENT
   Serial.println("BRIDGE_UPDATING");
-  
+
   digitalWrite(BLUE_LED, HIGH);
   delay(250);
   digitalWrite(GREEN_LED, HIGH);
@@ -262,51 +284,57 @@ void handleUpdate() {
 
   BearSSL::WiFiClientSecure wifi_update_client;
   bool mfln = wifi_update_client.probeMaxFragmentLength("www.github.com", 443, 1024);
-  if (mfln) {
+  if (mfln)
+  {
     wifi_update_client.setBufferSizes(1024, 1024);
   }
   wifi_update_client.setCertStore(&certStore);
   wifi_update_client.setInsecure();
 
   HTTPClient https_client;
-  if (https_client.begin(wifi_update_client, UPDATE_URL)) {
+  if (https_client.begin(wifi_update_client, UPDATE_URL))
+  {
     Serial.println("HTTPS connection success");
     int http_code = https_client.GET();
     Serial.print("HTTP code: ");
     Serial.println(http_code);
   }
   // https_client.setFollowRedirects(true);
-   
+
   t_httpUpdate_return update_ret = ESPhttpUpdate.update(wifi_update_client, UPDATE_URL);
-  switch (update_ret) {
-    case HTTP_UPDATE_FAILED:
-      Serial.println("BRIDGE_UPDATE_FAILED");
-      Serial.println(ESPhttpUpdate.getLastErrorString().c_str());
-      server.send(200, "text/html", UPDATE_FAILED_HTML);
-      break;
-    case HTTP_UPDATE_NO_UPDATES:
-      Serial.println("BRIDGE_NO_UPDATES_AVAILABLE");
-      server.send(200, "text/html", NO_UPDATES_AVAILABLE_HTML);
-      break;
-    case HTTP_UPDATE_OK:
-      // might not get called because the update function reboots the ESP..
-      Serial.println("BRIDGE_UPDATE_SUCCESS");
-      server.send(200, "text/html", UPDATE_SUCCESS_HTML);
-      break;
+  switch (update_ret)
+  {
+  case HTTP_UPDATE_FAILED:
+    Serial.println("BRIDGE_UPDATE_FAILED");
+    Serial.println(ESPhttpUpdate.getLastErrorString().c_str());
+    server.send(200, "text/html", UPDATE_FAILED_HTML);
+    break;
+  case HTTP_UPDATE_NO_UPDATES:
+    Serial.println("BRIDGE_NO_UPDATES_AVAILABLE");
+    server.send(200, "text/html", NO_UPDATES_AVAILABLE_HTML);
+    break;
+  case HTTP_UPDATE_OK:
+    // might not get called because the update function reboots the ESP..
+    Serial.println("BRIDGE_UPDATE_SUCCESS");
+    server.send(200, "text/html", UPDATE_SUCCESS_HTML);
+    break;
   }
   digitalWrite(BLUE_LED, LOW);
   digitalWrite(GREEN_LED, LOW);
   digitalWrite(RED_LED, LOW);
 }
 
-bool connectToTCPServer() {
+bool connectToTCPServer()
+{
   unsigned long tcp_connect_start_time = millis();
   unsigned long tcp_connect_current_time = millis();
   unsigned long tcp_connect_elapsed_time = 0;
   tcp_client.connect(TINET_HUB_HOST, TINET_HUB_PORT);
-  while (!tcp_client.connected()) {
+  while (!tcp_client.connected())
+  {
     tcp_connect_elapsed_time = tcp_connect_current_time - tcp_connect_start_time;
-    if (tcp_connect_elapsed_time > 5000) {
+    if (tcp_connect_elapsed_time > 5000)
+    {
       // timed out
       return false;
     }
@@ -314,14 +342,18 @@ bool connectToTCPServer() {
   return true;
 }
 
-void handleTCPToSerial() {
-  if (tcp_client.connected()) {
+void handleTCPToSerial()
+{
+  if (tcp_client.connected())
+  {
     int available_in_tcp = tcp_client.available();
 
-    if (available_in_tcp > 0) {
+    if (available_in_tcp > 0)
+    {
       flashLED(BLUE_LED, 10);
-      
-      while (tcp_client.available()) {
+
+      while (tcp_client.available())
+      {
         char c = tcp_client.read();
         Serial.write(c);
       }
@@ -329,46 +361,62 @@ void handleTCPToSerial() {
   }
 }
 
-void handleSerialToTCP() {
-  if (Serial.available() != 0) {
+void handleSerialToTCP()
+{
+  if (Serial.available() != 0)
+  {
     flashLED(BLUE_LED, 10);
     String serial_data = Serial.readStringUntil('\n');
-    
-    if (!tcp_client.connected() && serial_data == "CONNECT_TCP") {
-      if (connectToTCPServer()) {
+
+    if (!tcp_client.connected() && serial_data == "CONNECT_TCP")
+    {
+      if (connectToTCPServer())
+      {
         Serial.println("TCP_CONNECTED");
-      } else {
+      }
+      else
+      {
         Serial.println("TCP_CONNECT_TIMED_OUT");
       }
-    } else if (serial_data == "GET_TIME") {
+    }
+    else if (serial_data == "GET_TIME")
+    {
       Serial.print("CURRENT_TIME:");
       Serial.println(timeClient.getFormattedTime());
-    } else if (tcp_client.connected()) {
+    }
+    else if (tcp_client.connected())
+    {
       tcp_client.println(serial_data);
-    } else if (tcp_client.connected() && serial_data == "CONNECT_TCP") {
+    }
+    else if (tcp_client.connected() && serial_data == "CONNECT_TCP")
+    {
       Serial.println("TCP_ALREADY_CONNECTED");
     }
-    
+
     serial_data = "";
   }
 }
 
-void loadSettings() {
+void loadSettings()
+{
   EEPROM.get(0, settings);
 }
 
-void saveSettings() {
+void saveSettings()
+{
   EEPROM.put(0, settings);
   EEPROM.commit();
 }
 
-void resetToFactorySettings() {
+void resetToFactorySettings()
+{
   memset(&settings, 0, sizeof(settings));
   EEPROM.put(0, settings);
   EEPROM.commit();
 }
 
-void flashLED(uint8_t led_to_flash, int delay_to_flash) {
+void flashLED(uint8_t led_to_flash, int delay_to_flash)
+{
   digitalWrite(led_to_flash, HIGH);
   delay(delay_to_flash);
   digitalWrite(led_to_flash, LOW);
